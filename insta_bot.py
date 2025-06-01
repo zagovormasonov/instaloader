4f39bfaf42119f226b9472e8015c79b0175433bd
noName <samyrize77777@gmail.com>
2025-06-01 05:20:41 +0400

    payment

--- insta_bot.py
+++ insta_bot.py
@@ -161,4 +161,7 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
